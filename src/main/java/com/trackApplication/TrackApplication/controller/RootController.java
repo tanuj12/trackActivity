8f60b7d24a9fb4dc5d178ca77b2e57e66ca96c57
Tanuj Kulkarni <tanuj21199@gmail.com>
2025-02-13 23:01:43 -0500

    Added database connection

--- src/main/java/com/trackApplication/TrackApplication/controller/RootController.java
+++ src/main/java/com/trackApplication/TrackApplication/controller/RootController.java
@@ -1,12 +1,10 @@
 package com.trackApplication.TrackApplication.controller;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
-    
     @RequestMapping("/")
     public String rootPage() {
         return "Hellow";
